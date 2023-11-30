40cac8dd6ba51244a72ad57128f6f03ba0a2d4af
dahniardd <dahniardvn09@gmail.com>
2023-11-30 09:04:32 +0700

    Modifikasi Percobaan 2

--- Percobaan206.java
+++ Percobaan206.java
@@ -2,18 +2,30 @@ import java.util.Scanner;
 public class Percobaan206 {
     static int hitungPangkat (int x, int y) {
         if (y == 0) {
-            return (1);
+            return 1;
         } else {
             return (x * hitungPangkat(x, y - 1));
-        } 
+        }
     }
+    static void cetakDeretPangkat(int x, int y){
+        if (y == 0){
+            System.out.println("1");
+        } else {
+            System.out.print(x + "x");
+            cetakDeretPangkat(x, y - 1);
+    }
+}
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-            int bilangan, pangkat;
-            System.out.print("Bilangan yang dihitung : ");
-            bilangan = sc.nextInt();
-            System.out.print("Pangkat : ");
-            pangkat = sc.nextInt();
-            System.out.println(hitungPangkat(bilangan, pangkat));
+        int bilangan, pangkat;
+        System.out.print("Bilangan yang dihitung : ");
+        bilangan = sc.nextInt();
+        System.out.print("Pangkat : ");
+        pangkat = sc.nextInt();
+    System.out.println("Hasil perpangkatan : " +
+        hitungPangkat(bilangan, pangkat)); 
+        
+        cetakDeretPangkat(bilangan, pangkat);
         }
+        
 }
